6e3cae4c9878725bc6ee9ab287a9d873cb266325
Ahmed Saheed <ahmedsaheed2@outlook.com>
2022-10-05 20:36:23 +0100

    init .files

---
+++ fish/alias.fish
@@ -0,0 +1,11 @@
+alias vim=nvim
+alias cat=bat
+alias reload='exec fish'
+alias weather "curl https://v2.wttr.in/u"
+alias ip "curl ifconfig.me"
+alias fishProfile="nvim ~/.config/fish/config.fish"
+alias c="code ."
+alias griffith "cd $home && cd Desktop/Desktop/Griffith/stage3"
+alias desktop "cd $home && cd Desktop/Desktop"
+alias commander "cd $home && cd GolandProjects/commander/ && go run ."
+alias ls "exa --long --header"
