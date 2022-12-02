58b1e961c1952651b474193dc36486bc1ecc7f60
Ahmed Saheed <ahmedsaheed2@outlook.com>
2022-12-02 19:56:39 +0000

    reconfigure tide

--- fish/alias.fish
+++ fish/alias.fish
@@ -12,6 +12,8 @@ alias commander "cd $home && cd GolandProjects/commander/ && go run ."
 alias ls "exa --long --header"
 alias tdie "tmux kill-server"
 alias unx "cd $home && /usr/local/bin/uxn\ 2/uxnemu ; exit; "
+alias javaPrettier 'prettier --write "**/*.java"'
+alias python "python3"
 # create alias for clearing terminal which set TERM to xterm
 #alias clear="TERM xterm; clear;"
 
